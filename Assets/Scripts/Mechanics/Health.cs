332649a5278f33cd7bdc388dc9713d608548f285
c.nisenbaum@unity3d.com <>
2021-11-02 18:14:29 -0500

    refactor health.cs

--- Assets/Scripts/Mechanics/Health.cs
+++ Assets/Scripts/Mechanics/Health.cs
@@ -30,19 +30,6 @@ namespace Platformer.Mechanics
             currentHP = Mathf.Clamp(currentHP + 1, 0, maxHP);
         }
 
-        /// <summary>
-        /// Decrement the HP of the entity. Will trigger a HealthIsZero event when
-        /// current HP reaches 0.
-        /// </summary>
-        public void Decrement()
-        {
-            currentHP = Mathf.Clamp(currentHP - 1, 0, maxHP);
-            if (currentHP == 0)
-            {
-                var ev = Schedule<HealthIsZero>();
-                ev.health = this;
-            }
-        }
 
         /// <summary>
         /// Decrement the HP of the entitiy until HP reaches 0.
@@ -55,6 +42,20 @@ namespace Platformer.Mechanics
         void Awake()
         {
             currentHP = maxHP;
+        }
+        
+        /// <summary>
+        /// Decrement the HP of the entity. Will trigger a HealthIsZero event when
+        /// current HP reaches 0.
+        /// </summary>
+        public void Decrement()
+        {
+            currentHP = Mathf.Clamp(currentHP - 1, 0, maxHP);
+            if (currentHP == 0)
+            {
+                var ev = Schedule<HealthIsZero>();
+                ev.health = this;
+            }
         }
     }
 }
